f32e5644956dd65b8bebb118606c8c79e09d0e42
tristanRuns <tristansantos@pobox.com>
2018-04-26 20:56:45 -0700

    Map Fragment code

--- app/src/main/java/danscal/coffeejournals/MapViewFragment.java
+++ app/src/main/java/danscal/coffeejournals/MapViewFragment.java
@@ -1,21 +1,121 @@
 package danscal.coffeejournals;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MapViewFragment extends Fragment {
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class MapViewFragment extends  Fragment /*implements OnMapReadyCallback*/ {
     private static final String TAG = "MapViewFragment";
+    FirebaseListAdapter mAdapter;
+    FirebaseAuth mAuth;
+    DatabaseReference mRef;
+   // private SupportMapFragment map;
+    MapView mapView;
+    //private GoogleMap googleMap;
+    MapView mMapView;
+    private GoogleMap googleMap;
 
+
+    private GoogleMap mMap;
     @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.map_view_fragment, container, false);
 
-        return view;
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.map_view_fragment, container, false);
+
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume(); // needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference().child("coffee shops");
+
+
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+               CoffeeShop shop=new CoffeeShop();
+
+                for(int i=0; i<100;i++) {
+                    // For showing a move to my location button
+                    //googleMap.setMyLocationEnabled(true);
+                    LatLng sydney = new LatLng(10+i, 10+i);
+                    googleMap.addMarker(new MarkerOptions().position(sydney).title("Hi").snippet("Marker Description"));
+
+
+                }
+
+
+                    // For dropping a marker at a point on the Map
+                    LatLng sydney = new LatLng(-34, 151);
+                    googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                    // For zooming automatically to the location of the marker
+                    //CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(8).build();
+                    //googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
